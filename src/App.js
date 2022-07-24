0445d3c7a0bc1e3021faecb2dacb176bf3fd60ac
Ivan Castellanos <ivan@ivant.dev>
2022-07-23 19:20:02 -0500

    Manejo de efectos React en useLocalStorage

--- src/App.js
+++ src/App.js
@@ -39,33 +39,52 @@ const sortTodos = (todoList) => {
 };
 
 function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  const [ error, setError] = React.useState(false);
+  const [ loading, setLoading ] = React.useState(true);
+  const [ item, setItem ] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = [];
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
   
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = [];
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
-  
-  const [ item, setItem ] = React.useState(parsedItem);
+        setItem(parsedItem);
+        setLoading(false);
+      } catch(error) {
+        setError(error);
+      }
+    }, 1000);
+  });
 
   const saveItem = (newItem) => {
-    const strigifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, strigifiedItem);
-
-    setItem(newItem);
+    try {
+      const strigifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, strigifiedItem);
+  
+      setItem(newItem);
+    } catch(error) {
+      setError(error);
+    }
   };
 
-  return [ 
+  return { 
     item,
-    saveItem
-  ];
+    saveItem,
+    loading,
+    error,
+  };
 }
 
 function App() {
-  const [ todos, saveTodos ] = useLocalStorage("TODOS_V1", []);
+  const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => (!!todo.completed)).length;
@@ -103,10 +122,20 @@ function App() {
     saveTodos(newTodos);
   };
 
+  // console.log("render (antes del useEffect)")
+
+  // React.useEffect(() => {
+  //   console.log('use effect')
+  // }, []);
+
+  // console.log("render (despues del useEffect)")
+
   return (
     <AppUI 
-      total={totalTodos}
-      completed={completedTodos}
+      loading={loading}
+      error={error}
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
       searchValue={searchValue} 
       setSearchValue={setSearchValue}
       searchedTodos={searchedTodos}
--- src/AppUI.js
+++ src/AppUI.js
@@ -9,37 +9,44 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
 import { TodoHeader } from "./TodoHeader";
-function AppUI( { totalTodos, completedTodos, searchValue, setSearchValue, searchedTodos, completeTodos, deleteTodos } ) {
-    return (
-        <React.Fragment>
-            <TodoHeader />
-            <div className="main__container">
-            <TodoCounter 
-                total={totalTodos}
-                completed={completedTodos}
+function AppUI({
+  error,
+  loading,
+  totalTodos,
+  completedTodos,
+  searchValue,
+  setSearchValue,
+  searchedTodos,
+  completeTodos,
+  deleteTodos,
+}) {
+  return (
+    <React.Fragment>
+      <TodoHeader />
+      <div className="main__container">
+        <TodoCounter total={totalTodos} completed={completedTodos} />
+
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+
+        <TodoList>
+          {error && <p className="main__info">Oops, error...</p>}
+          {loading && <p className="main__info">Working on...</p>}
+          {!loading && !searchedTodos.length && <p className="main__info">Create your first ToDo!</p>}
+          {searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              priority={todo.priority}
+              onComplete={() => completeTodos(todo.text)}
+              onDelete={() => deleteTodos(todo.text)}
             />
-        
-            <TodoSearch 
-                searchValue={searchValue} 
-                setSearchValue={setSearchValue} 
-            />
-        
-            <TodoList>
-                { searchedTodos.map((todo) => (
-                <TodoItem 
-                    key={todo.text}
-                    text={todo.text}
-                    completed={todo.completed}
-                    priority={todo.priority}
-                    onComplete={() => completeTodos(todo.text)}
-                    onDelete={() => deleteTodos(todo.text)}
-                />
-                ))}
-            </TodoList>
-            <CreateTodoButton />
-            </div>
-        </React.Fragment>
-        );
+          ))}
+        </TodoList>
+        <CreateTodoButton />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export { AppUI };
